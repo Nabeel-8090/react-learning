8de7fa49a90b3529f8dc4240e60b9a0dc2eca241
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2026-08-18 20:42:22 +0500

    fix minor issues

--- 06-React-Routing/ecommerce-project/src/pages/CheckoutPage.jsx
+++ 06-React-Routing/ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,5 +1,5 @@
-import './CheckoutPage.css';
 import './checkout-header.css';
+import './CheckoutPage.css';
 
 function CheckoutPage() {
     return (
--- 06-React-Routing/ecommerce-project/src/pages/HomePage.jsx
+++ 06-React-Routing/ecommerce-project/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import './HomePage.css';
 import './header.css';
+import './HomePage.css';
 
 function HomePage() {
     return (
