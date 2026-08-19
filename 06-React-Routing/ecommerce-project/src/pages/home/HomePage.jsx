ae9a6930ac391db80e210683f9a213c7dc8f854c
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2026-08-19 20:40:00 +0500

    create add to cart feature

--- 06-React-Routing/ecommerce-project/src/pages/home/HomePage.jsx
+++ 06-React-Routing/ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 import ProductsGrid from './ProductsGrid';
 import './HomePage.css';
 
-function HomePage({ cart }) {
+function HomePage({ cart, loadCart }) {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
@@ -26,7 +26,8 @@ function HomePage({ cart }) {
 
             <div className="home-page">
                 <ProductsGrid
-                    products={products} 
+                    products={products}
+                    loadCart={loadCart}
                 />
             </div>
         </>
