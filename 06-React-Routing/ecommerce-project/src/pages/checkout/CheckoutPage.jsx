6932bbc18286abc8a30092e087694b2015938a93
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2026-08-20 11:55:40 +0500

    make create order button work

--- 06-React-Routing/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ 06-React-Routing/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -40,6 +40,7 @@ function CheckoutPage({ cart, loadCart }) {
                     />
                     <PaymentSummary
                         paymentSummary={paymentSummary}
+                        loadCart={loadCart}
                     />
                 </div>
             </div>
